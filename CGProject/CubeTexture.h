7065e79113f508ab394b524168b74291d13f5d81
Chun2Maru <57833134+Chun2Maru@users.noreply.github.com>
2022-05-16 21:41:36 +0700

    Room done

---
+++ CGProject/CubeTexture.h
@@ -0,0 +1,25 @@
+#ifndef CUBE_TEXTURE_CLASS_H
+#define CUBE_TEXTURE_CLASS_H
+
+#include<glad/glad.h>
+#include<stb/stb_image.h>
+
+#include"shaderClass.h"
+
+class CubeTexture
+{
+public:
+	GLuint ID;
+	GLenum type;
+	CubeTexture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+	// Assigns a texture unit to a texture
+	void texUnit(Shader& shader, const char* uniform, GLuint unit);
+	// Binds a texture
+	void Bind();
+	// Unbinds a texture
+	void Unbind();
+	// Deletes a texture
+	void Delete();
+};
+#endif
